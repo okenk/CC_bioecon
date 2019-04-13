2c2917a5e20afd0f92344fba7c3fd4b59366e791
okenk <kiva.oken@gmail.com>
2019-04-12 17:21:40 -0700

    Got model running! It is bad.

--- Code/organize_plot_results.R
+++ Code/organize_plot_results.R
@@ -1,17 +1,22 @@
-require(reshape2)
-require(ggplot2)
+library(reshape2)
+library(ggplot2)
 
 Catch.df <- melt(Catch, value.name = 'catch') %>%
-  left_join(tibble(spp, price, cost)) %>%
-  left_join(melt(wt_at_rec, value.name = 'weight')) %>%
-  mutate(profit = ifelse(catch > 0, catch * weight * price - cost, 0)) %>%
-  as_tibble
+  left_join(melt(N[,,1], value.name = 'recruitment'))
 
 filter(Catch.df) %>%
   ggplot() +
-  geom_line(aes(x=wk, y=catch, group=yr)) +
-  facet_grid(fleet ~ spp, scales = 'free_y') +
+  geom_line(aes(x=wk, y=catch, group=yr, col = recruitment), alpha = .5) +
+  facet_grid(spp ~ fleet, scales = 'free_y') +
+  # geom_vline(xintercept = 18) +
+  # ylim(0, .05) +
   NULL
 
+# Salmon questions: Some years ships drop off one by one, other years altogether.
+# Some years effort stops even though catches were higher than in years when effort doesn't stop.
+# Why???
 
-filter(Catch.df, spp=='crab', fleet=='crab', wk==1)
+as_tibble(Catch.df) %>%
+  filter(spp == 'salmon', fleet == 'salmon') %>%
+  arrange(yr, wk) %>%
+  filter(catch > .015) %>% View
--- Code/toy_model.R
+++ Code/toy_model.R
@@ -1,8 +1,8 @@
-require(dplyr)
-require(mvtnorm)
-require(optimx)
+library(dplyr)
+library(mvtnorm)
 
-set.seed(8902809)
+
+# Model parameters --------------------------------------------------------
 
 spp <- c('crab', 'salmon')
 fleets <- c('crab', 'salmon', 'both')
@@ -25,7 +25,7 @@ pop_seasons['crab', 38:wks_per_yr] <- 0
 
 # salmon (spp. 2) season = May 1 - Oct. 31 (actually more complicated)
 pop_seasons['salmon', 1:21] <- 0
-pop_seasons['salmon', 18] <- 0.3
+pop_seasons['salmon', 22] <- 0.3
 pop_seasons['salmon', 47] <- 0.7
 pop_seasons['salmon', 48:wks_per_yr] <- 0;
 
@@ -37,33 +37,17 @@ price <- c(1, 1)
 avg_rec <- c(1, 1)
 avg_wt <- c(1,1)
 
-weight_cv <- 0.2
-recruit_cv <- 0.2
+weight_cv <- sqrt(log(0.2^2+1))
+recruit_cv <- sqrt(log(0.2^2+1))
 
 fixed_costs <- c(.0025, .0001)
 cost_per_trip <- rep(NA, 2)
 cost_cv <- sqrt(log(.15^2+1))
+cost_corr <- 0.8
 
 names(catchability) <- names(price) <- names(avg_rec) <- names(avg_wt) <- names(fixed_costs) <- names(cost_per_trip) <- spp
 
-Catch <- array(0, dim = c(npops, nyrs, wks_per_yr, nfleets), 
-               dimnames = list(spp = spp, yr = NULL, wk = NULL, fleet = fleets))
-N <- array(0, dim = c(npops, nyrs, wks_per_yr), dimnames = list(spp = spp, yr = NULL, wk = NULL))
-N[,,1] <- rmvnorm(nyrs, mean = log(avg_rec), sigma = recruit_cv * diag(2)) %>% 
-  t %>% 
-  exp
-# recruit CV = 20%
-
-wt_at_rec <- rmvnorm(nyrs, mean = log(avg_rec), sigma = weight_cv * diag(2)) %>% 
-  t %>%
-  exp
-# Right now: white noise, independence between stocks. 
-# To do: correlation in time and between stocks.
-
-
-salmon_tac_rule <- 0.2
-salmon_tac <- N['salmon',,1] * salmon_tac_rule
-# TAC for troll fishery is 20% of recruitment? (arbitrary) 
+salmon_tac_rule <- 0.3
 
 # avg_rev <- numeric(2)
 # names(avg_rev) <- spp
@@ -74,6 +58,8 @@ salmon_tac <- N['salmon',,1] * salmon_tac_rule
 
 # fixed_costs <- c(0.3, 0.1) * avg_rev
 # crab 30% of revenue, salmon 10% of revenue
+
+# Calculating variable costs ----------------------------------------------
 
 calc_var_cost <- function(log_avg_cost_per_trip, cost_cv, recruits, wt_at_rec, fishing_season, in_season_dpltn, 
                           fleet_size, fixed_costs, catchability, price, tac = NA) {
@@ -119,36 +105,89 @@ xx <- uniroot(calc_var_cost, interval = c(-20, 0),
               fixed_costs = .0001, catchability = catchability['salmon'], tac = salmon_tac_rule)
 cost_per_trip['salmon'] <- exp(xx$root)
 
+# initialize profit array with fixed costs
 
+
+# Set up objects to fill --------------------------------------------------
+set.seed(8902809)
+
+profits <- array(-t(fleet_permits) %*% fixed_costs, dim = c(nfleets, max(ships_per_fleet), nyrs),  
+                 dimnames = list(fleet = fleets, ship = NULL, yr = NULL)) %>%
+  aperm(c(2,3,1))
+
+Catch <- array(0, dim = c(npops, nyrs, wks_per_yr, nfleets), 
+               dimnames = list(spp = spp, yr = NULL, wk = NULL, fleet = fleets))
+
+N <- array(0, dim = c(npops, nyrs, wks_per_yr), dimnames = list(spp = spp, yr = NULL, wk = NULL))
+N[,,1] <- rmvnorm(nyrs, mean = log(avg_rec), sigma = recruit_cv * diag(npops)) %>% 
+  t %>% 
+  exp
+# recruit CV = 20%
+
+wt_at_rec <- rmvnorm(nyrs, mean = log(avg_rec), sigma = weight_cv * diag(npops)) %>% 
+  t %>%
+  exp
+dimnames(wt_at_rec) <- list(spp = spp, yr = NULL)
+# Right now: white noise, independence between stocks. 
+# To do: correlation in time and between stocks.
+
+salmon_tac <- N['salmon',,1] * salmon_tac_rule
+# TAC for troll fishery is 20% of recruitment? (arbitrary) 
 
 # simulate cost per trip for each ship in each fleet
-cost_by_ship <- array(NA, dim = c(max(ships_per_fleet), nfleets, npops), dimnames = list(ships = NULL, fleet = fleets, spp = spp))
+cost_by_ship <- array(0, dim = c(max(ships_per_fleet), nfleets, npops), 
+                      dimnames = list(ships = NULL, fleet = fleets, spp = spp))
+sigma_mat <- matrix(cost_corr*cost_cv^2, nrow = npops, ncol = npops)
+diag(sigma_mat) <- cost_cv^2
+
 for(fleet in 1:nfleets) {
-  fleet_permits_logical <- as.logical(fleet_permits[,fleet])
-  cost_by_ship[1:ships_per_fleet[fleet], fleet, fleet_permits_logical] <- sapply(cost_per_trip[fleet_permits_logical], 
-                                                                                 function(cost) rlnorm(ships_per_fleet[fleet], log(cost), cost_cv))
+  fleet_permits_TF <- as.logical(fleet_permits[,fleet])
+  cost_by_ship[1:ships_per_fleet[fleet], fleet,] <- exp(rmvnorm(ships_per_fleet[fleet], mean = log(cost_per_trip), 
+                                                                sigma = sigma_mat)) %*% 
+    diag(fleet_permits_TF)
 }
+cost_by_ship[cost_by_ship==0] <- NA
+
+
+# Run model! --------------------------------------------------------------
 
 for(yr in 1:nyrs) {
+  temp_catchability <- catchability
   for(wk in 1:wks_per_yr) {
-    exp_rev <- array(N[,yr,wk] * wt_at_rec[,yr] * catchability * price * pop_seasons[,wk], dim=c(2, 100, 3)) %>% 
-      aperm(perm = c(2,3,1))
-    # if out of legal season, expected revenue = 0, no one will fish.
-    
-    exp_profit <- exp_rev - cost_by_ship
-    best_spp <- apply(exp_profit, c(1,2), function(ship) ifelse(max(ship, na.rm=TRUE) >= 0, which.max(ship), NA))
-    # returns index of most profitable stock for each ship, or NA if no stock is profitable
-    
-    ships_per_stock <- apply(best_spp, 2, tabulate, nbins = npops)
-    # counts up number of ships fishing each stock
+      if(sum(pop_seasons[,wk]) > 0) {
+      exp_rev <- array(N[,yr,wk] * wt_at_rec[,yr] * temp_catchability * price * pop_seasons[,wk], 
+                       dim=c(npops, max(ships_per_fleet), nfleets)) %>% 
+        aperm(perm = c(2,3,1))
+      # If out of legal season, expected revenue = 0. 
+      # exp_rev does *not* care whether a fleet has a permit though! (That's in exp_profit)
+      
+      exp_profit <- exp_rev - cost_by_ship
+      
+      best_spp <- apply(exp_profit, c(1,2), function(ship) ifelse(max(ship, na.rm=TRUE) > 0, 
+                                                                  which.max(ship), NA))
+      # returns index of most profitable stock for each ship, or NA if no stock is profitable. Slow step!
+      
+      if(any(!is.na(best_spp))) {
+        ships_per_stock <- apply(best_spp, 2, tabulate, nbins = npops)
+      } else {
+        matrix(0, nrow = npops, ncol = nfleets)
+      }
+      # counts up number of ships fishing each stock
 
-    # Next task: keep track of costs and revenues
+      profits[,yr,] <- profits[,yr,] + apply(exp_profit, c(1,2), function(ship) max(c(ship, 0), na.rm = TRUE))
+      # actual profits are for the stock with max expected profit, if max is > 0. Kind of slow.
+      
+      Catch[,yr,wk,] <-  t(t(ships_per_stock * temp_catchability * N[,yr,wk]) %*% diag(wt_at_rec[,yr]))
+    } else {
+      Catch[,yr,wk,] <- 0
+    }
     
-    Catch[,yr,wk,] <-  ships_per_stock * catchability * N[,yr,wk]
     if(wk < wks_per_yr) {
       N[,yr,wk+1] <- N[,yr,wk] - apply(Catch[,yr,wk,], 1, sum)
     }
+    if(sum(Catch['salmon',yr,,]) >= salmon_tac[yr]) {
+      # if you hit the salmon TAC, set catchability to 0
+      temp_catchability['salmon'] <- 0
+    }
   }
 }
-
-save.image(file = 'code/base_conditions.RData')
