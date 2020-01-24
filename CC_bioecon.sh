cd49025cd7ba76359cc9712f36d04a89346155ca
Kiva Laren Oken <kloken@farm.cse.ucdavis.edu>
2020-01-24 15:58:56 -0800

    working on farm

--- CC_bioecon.sh
+++ CC_bioecon.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH -D /home/kloken/CC_bioecon
-#SBATCH -o /home/kloken/CC_bioecon/slurm_log/output_%a.txt
+#SBATCH -o /home/kloken/CC_bioecon/slurm_log/output%a.txt
 #SBATCH -e /home/kloken/CC_bioecon/slurm_log/error%a.txt
 #SBATCH -J bioecon_sim
-#SBATCH -t 01:00:00
+#SBATCH -t 48:00:00
 #SBATCH --array=0-2
 
 scenarios=( 'synchrony' 'access' 'interaction')
 
 scenario=${scenarios[$SLURM_ARRAY_TASK_ID]}
 
-Rscript ~/CC_bioecon/Code/for_cluster/${scenario}_sims.R 1000
+Rscript ~/CC_bioecon/Code/for_cluster/${scenario}_sims.R 10000
