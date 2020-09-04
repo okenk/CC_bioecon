8b2bc82697fa66c9731f27921104d21195169aba
okenk <kiva.oken@gmail.com>
2020-09-04 10:34:42 -0700

    Reviewer responses and some presentation figures

--- Code/PICES.R
+++ Code/PICES.R
@@ -1,13 +1,14 @@
+library(tidyverse)
+
+  
 make_pices_plots <- function(tibble.list, folder, sim.file.name, sim.id) {
   list2env(tibble.list, sys.frame(sys.nframe()))
+  theme_set(ggsidekick::theme_sleek(base_size = 14)) 
+  
   total.summary <- spread(tibble.list$total.summary, key = metric, value = value)  
   p.mn <- ggplot(total.summary) +
     geom_density(aes(x = revenue.mn, col = get(sim.id), fill = get(sim.id)), alpha = 0.25) +
     guides(fill = guide_legend(title = sim.id), col = guide_legend(title = sim.id)) +
-    theme_bw(base_size = 14) +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          strip.background = element_rect(fill="white")) +
     xlab('Mean revenue') +
     scale_color_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
     scale_fill_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
@@ -16,10 +17,6 @@ make_pices_plots <- function(tibble.list, folder, sim.file.name, sim.id) {
   p.cv <-  ggplot(total.summary) +
     geom_density(aes(x = revenue.cv, col = get(sim.id), fill = get(sim.id)), alpha = 0.25) +
     guides(fill = guide_legend(title = sim.id), col = guide_legend(title = sim.id)) +
-    theme_bw(base_size = 14) +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          strip.background = element_rect(fill="white")) +
     xlab('Revenue CV') +
     scale_color_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
     scale_fill_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
@@ -31,12 +28,8 @@ make_pices_plots <- function(tibble.list, folder, sim.file.name, sim.id) {
   
   to.save <- ggplot(revenue.df.spp) +
     geom_density(aes(x = revenue.mn, col = get(sim.id), fill = get(sim.id)), alpha = 0.25) +
-    facet_wrap(~spp, nrow = 1, scales = "free") +
+    facet_wrap(~str_to_title(spp), nrow = 1, scales = "free") +
     guides(fill = guide_legend(title = sim.id), col = guide_legend(title = sim.id)) +
-    theme_bw(base_size = 14) +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          strip.background = element_rect(fill="white")) +
     xlab('Mean revenue') +
     scale_color_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
     scale_fill_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
@@ -46,12 +39,8 @@ make_pices_plots <- function(tibble.list, folder, sim.file.name, sim.id) {
   
   to.save <- ggplot(revenue.df.spp) +
     geom_density(aes(x = revenue.cv, col = get(sim.id), fill = get(sim.id)), alpha = 0.25) +
-    facet_wrap(~spp, nrow = 1, scales = "free") +
+    facet_wrap(~str_to_title(spp), nrow = 1, scales = "free") +
     guides(fill = guide_legend(title = sim.id), col = guide_legend(title = sim.id)) +
-    theme_bw(base_size = 14) +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          strip.background = element_rect(fill="white")) + 
     xlab('Revenue CV') +
     scale_color_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
     scale_fill_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1)]) +
@@ -60,5 +49,11 @@ make_pices_plots <- function(tibble.list, folder, sim.file.name, sim.id) {
          units = 'in', dpi = 500)
 }
 
-make_pices_plots(sync_tibbles, 'PICES', 'sync_3_spp', 'synchrony')
-make_pices_plots(access_tibbles, '10k', 'access_3_spp', 'access')
+load('Data/10k_5_20/sync_df_10-8_1k.RData')
+load('Data/10k_5_20/access_df_10-8_1k.RData')
+
+make_pices_plots(sync_tibbles, 'Presentation', 'sync', 'synchrony')
+make_pices_plots(access_tibbles, 'Presentation', 'access', 'access')
+
+make_half_baked_plots(sync_tibbles, 'Presentation', 'sync', 'synchrony')
+make_half_baked_plots(access_tibbles, 'Presentation', 'access', 'access')
