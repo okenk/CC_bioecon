fd2ef5a163ce308e5e5748cb0bd70c84765d5297
okenk <kiva.oken@gmail.com>
2020-11-13 11:26:04 -0800

    SWFSC talk figures

--- Code/PICES.R
+++ Code/PICES.R
@@ -57,3 +57,65 @@ make_pices_plots(access_tibbles, 'Presentation', 'access', 'access')
 
 make_half_baked_plots(sync_tibbles, 'Presentation', 'sync', 'synchrony')
 make_half_baked_plots(access_tibbles, 'Presentation', 'access', 'access')
+
+
+# portfolio benefit -------------------------------------------------------
+pal <- wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1,2)]
+names(pal) <- c('-0.5', '0', '0.5')
+
+base.plot <- sync_tibbles$income.summary %>%
+  prep_individuals_to_plot(experiment = 'synchrony') %>%
+  group_by(synchrony, fleet) %>%
+  summarize_at('revenue.cv', .funs = list(v.low = ~ quantile(., 0.025), low = ~ quantile (., 0.25), 
+                                          mid = median, high = ~ quantile(., 0.75),
+                                          v.high = ~ quantile(., 0.975))) %>%
+  pivot_longer(cols = v.low:v.high, names_to = 'quantile', values_to = 'value') %>%
+  filter(fleet != 'Salmon', fleet != 'Groundfish') %>%
+  pivot_wider(names_from = fleet, values_from = value) %>%
+  mutate_at(vars(`Crab-Salmon`:`Crab-Salmon-\nGroundfish`), .funs = list(~ Crab/.)) %>%
+  select(-Crab) %>%
+  pivot_longer(cols = `Crab-Salmon`:`Crab-Salmon-\nGroundfish`, names_to = 'fleet',
+               values_to = 'portfolio_benefit') %>%
+  mutate(quantile = recode(quantile, v.low = .025, low = .25, mid = .5, high = .75, v.high = .975),
+         fleet = fct_inorder(str_to_title(fleet))) %>%
+  ggplot(aes(x = quantile, y = portfolio_benefit, col = synchrony)) +
+  geom_hline(yintercept = 1, lty = 2) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~ fleet, ncol = 3) +
+  ylim(0.25, 1.75) +
+  ggsidekick::theme_sleek(base_size = 14) +
+  scale_color_manual(values = pal, name = 'Synchrony') +
+  scale_x_continuous(name = 'Revenue CV quantile', labels = c('0', '0.25', '0.5', '0.75', '1.0')) +
+  # xlab('Revenue CV quantile') +
+  ylab('Portfolio benefit') +
+  # labs(col = 'Synchrony') +
+  NULL
+
+base.plot %+%
+  filter(base.plot$data, synchrony == '0') %>%
+  ggsave(filename = 'Figures/Presentation/portfolio_benefits1.png', plot = ., height = 4, width = 9,
+         units = 'in', dpi = 500)
+
+ggsave('Figures/Presentation/portfolio_benefits2.png', base.plot, height = 4, width = 9,
+       units = 'in', dpi = 500)
+
+
+
+# Gini index --------------------------------------------------------------
+
+to.save <- access_tibbles$gini.index %>%
+  ungroup() %>%
+  mutate(access = str_to_title(access)) %>% 
+  mutate(access = str_replace(access, ' ', '\n')) %>%
+  group_by(access) %>%
+  summarize(mn = mean(gini),
+            upper = quantile(gini, probs = .975),
+            lower = quantile(gini, probs = 0.025)) %>%
+  ggplot() +
+  geom_col(aes(x = access, y = mn, fill = access)) +
+  geom_linerange(aes(x = access, ymin = lower, ymax = upper), col = 'gray40') +
+  ylim(0,1) +
+  labs(x = '', y = 'Gini index', fill = 'Access') +
+  scale_fill_manual(values = wesanderson::wes_palette('Zissou1', n = 5)[c(5,3,1,2)])
+ggsave('Figures/Presentation/gini.png', plot = to.save, dpi = 500, height = 5, width = 7, units = 'in')
